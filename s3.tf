da337a1f510911a55d48c26490e0434cb9edd3ac
amarin-dspace <131253145+amarin-dspace@users.noreply.github.com>
2024-02-29 11:54:53 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -9,3 +9,11 @@ resource "aws_s3_bucket_object" "object1" {
   key = each.value
   source = "uploads/${each.value}"
 }
+
+resource "aws_s3_bucket_public_access_block" "app" {
+  bucket = aws_s3_bucket.example-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
