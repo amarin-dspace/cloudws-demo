eab084334adae2540108ef4d06d879525377f0e2
amarin-dspace <131253145+amarin-dspace@users.noreply.github.com>
2024-02-29 11:54:13 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,3 +2,10 @@ resource "aws_s3_bucket" "example-bucket" {
    bucket = "wsdemo-s3"
    acl = "private"  
 }
+
+resource "aws_s3_bucket_object" "object1" {
+  for_each = fileset("uploads/", "*")
+  bucket = aws_s3_bucket.example-bucket.id
+  key = each.value
+  source = "uploads/${each.value}"
+}
