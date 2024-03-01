b44899db39694be3a34ee6a62e1e5d4b0757783a
amarin-dspace <131253145+amarin-dspace@users.noreply.github.com>
2024-03-01 13:32:56 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,4 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 resource "aws_instance" "example_server" {
   ami           = "ami-0a23a9827c6dab833"
   instance_type = "t2.micro"
