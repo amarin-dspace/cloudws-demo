0d466ca3ebfe435cbd74e766ed090f8f0ec1a3c4
amarin-dspace <131253145+amarin-dspace@users.noreply.github.com>
2024-03-01 13:08:25 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example_server" {
   ami           = "ami-0a23a9827c6dab833"
   instance_type = "t2.micro"
+  #associate_public_ip_address = false
 
   tags = {
     Name = "WS-Demo"
