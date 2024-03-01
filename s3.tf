0379a4b0aa7a236ad16fc304b948ee5b6284b661
amarin-dspace <131253145+amarin-dspace@users.noreply.github.com>
2024-03-01 13:11:29 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,27 @@
-resource "aws_s3_bucket" "example-bucket" {
+resource "aws_s3_bucket" "example" {
    bucket = "wsdemo-s3"
    acl = "private"  
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership]
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "demo" {
+  bucket = aws_s3_bucket.example.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # Uncomment if you want to upload files to S3 bucket using TF
@@ -11,10 +32,3 @@ resource "aws_s3_bucket" "example-bucket" {
 #  source = "uploads/${each.value}"
 }
 
-resource "aws_s3_bucket_public_access_block" "app" {
-  bucket = aws_s3_bucket.example-bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
